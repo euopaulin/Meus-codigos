ece928cbbf602f9531c60bb0d93e7614da56dfe4
Paulin <paulohspfc209@gmail.com>
2024-04-17 12:02:39 -0300

    Novos updates

---
+++ Python/teste 2.py
@@ -0,0 +1,7 @@
+print ('Olá, usuario. Seja bem vindo!')
+print = input ('Antes de iniciarmos me diga qual o seu nome, por favor: ')
+print = input 
+print = input ('Ok. E qual é a sua idade? ')
+print = input('Certo, agora você está liberado a usar o site. Fique a vontade!')
+
+print = input
